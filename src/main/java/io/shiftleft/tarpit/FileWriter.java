ab5a4c688f1b50e8c8d6c7b394d0146f05a3eb54
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-04-21 21:08:31 -0400

    Update FileWriter.java

--- src/main/java/io/shiftleft/tarpit/FileWriter.java
+++ src/main/java/io/shiftleft/tarpit/FileWriter.java
@@ -57,7 +57,8 @@ public class FileWriter extends HttpServlet {
 
     InputStream input = filePart.getInputStream();
 
-    File targetFile = new File(productSourceFolder + filePart.getSubmittedFileName());
+    String submittedFileName = filePart.getSubmittedFileName();
+    File targetFile = new File(productSourceFolder, submittedFileName);
 
     targetFile.createNewFile();
     OutputStream out = new FileOutputStream(targetFile);
@@ -78,4 +79,30 @@ public class FileWriter extends HttpServlet {
     doGet(request, response);
   }
 
-}
+public static void unzipFile(String zipFileWithAbsolutePath, String destination)
+      throws IOException {
+    if (!doesFileExists(zipFileWithAbsolutePath)) {
+      throw new FileNotFoundException("The given zip file not found: " + zipFileWithAbsolutePath);
+    }
+
+    String fileName = getFileFromPath(zipFileWithAbsolutePath);
+    String finalDestination = getFinalDestination(fileName, destination);
+    createDirectoryNamedAsZipFile(finalDestination);
+
+    try {
+      // Initiate ZipFile object with the path/name of the zip file.
+      ZipFile zipFile = new ZipFile(zipFileWithAbsolutePath);
+
+      // Extracts all files to the path specified
+      zipFile.extractAll(finalDestination);
+
+    } catch (ZipException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+private static boolean doesFileExists(String fileName) {
+    File f = new File(fileName);
+    return f.exists();
+  }
