107c4b2bb605081e266d72ba68e0e71d2be1d81d
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-06-04 01:13:44 -0400

    Update ServletTarPit.java

--- src/main/java/io/shiftleft/tarpit/ServletTarPit.java
+++ src/main/java/io/shiftleft/tarpit/ServletTarPit.java
@@ -63,11 +63,10 @@ public class ServletTarPit extends HttpServlet {
       engine.eval(request.getParameter("module"));
 
       /* FLAW: Insecure cryptographic algorithm (DES) 
-      Cipher aes = Cipher.getInstance("AES");
-      KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-      keyGen.init(128); // for example
-      SecretKey secretKey = keyGen.generateKey();
-      aes.init(Cipher.ENCRYPT_MODE, secretKey);
+      CWE: 327 Use of Broken or Risky Cryptographic Algorithm */
+      Cipher des = Cipher.getInstance("DES");
+      SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+      des.init(Cipher.ENCRYPT_MODE, key);
 
       getConnection();
 
