35ece4a479337576f6bdad5f83ec66da3cfcaffd
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-06-03 23:27:16 -0400

    Update ServletTarPit.java

--- src/main/java/io/shiftleft/tarpit/ServletTarPit.java
+++ src/main/java/io/shiftleft/tarpit/ServletTarPit.java
@@ -1,130 +1,96 @@
 package io.shiftleft.tarpit;
 
-import io.shiftleft.tarpit.model.User;
-import io.shiftleft.tarpit.DocumentTarpit;
-import java.io.IOException;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringEscapeUtils;
 
+public class ServletTarPit extends javax.servlet.http.HttpServlet {
 
-@WebServlet(name = "simpleServlet", urlPatterns = {"/vulns"}, loadOnStartup = 1)
-public class ServletTarPit extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(ServletTarPit.class.getName());
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
 
-  private static final long serialVersionUID = -3462096228274971485L;
-  private Connection connection;
-  private PreparedStatement preparedStatement;
-  private ResultSet resultSet;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
 
+        String ACCESS_KEY_ID = "AKIA2E0A8F3B244C9986";
+        String SECRET_KEY = "7CE556A3BC234CC1FF9E8A5C324C0BB70AA21B6D";
 
-  private final static Logger LOGGER = Logger.getLogger(ServletTarPit.class.getName());
+        String txns_dir = System.getProperty("transactions_folder","/rolling/transactions");
 
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        String encodedPath = request.getParameter("encodedPath");
 
-    String ACCESS_KEY_ID = "AKIA2E0A8F3B244C9986";
-    String SECRET_KEY = "7CE556A3BC234CC1FF9E8A5C324C0BB70AA21B6D";
+        String xxeDocumentContent = request.getParameter("entityDocument");
+        DocumentTarpit.getDocument(xxeDocumentContent);
 
-    String txns_dir = System.getProperty("transactions_folder","/rolling/transactions");
+        boolean keepOnline = (request.getParameter("keeponline") != null);
 
-    String login = request.getParameter("login");
-    String password = request.getParameter("password");
-    String encodedPath = request.getParameter("encodedPath");
+        LOGGER.info(" AWS Properties are " + ACCESS_KEY_ID + " and " + SECRET_KEY);
+        LOGGER.info(" Transactions Folder is " + txns_dir);
 
-    String xxeDocumentContent = request.getParameter("entityDocument");
-    DocumentTarpit.getDocument(xxeDocumentContent);
+        try {
 
-    boolean keepOnline = (request.getParameter("keeponline") != null);
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("JavaScript");
+            engine.eval(request.getParameter("module"));
 
-    LOGGER.info(" AWS Properties are " + ACCESS_KEY_ID + " and " + SECRET_KEY);
-    LOGGER.info(" Transactions Folder is " + txns_dir);
+            Cipher des = Cipher.getInstance("DES");
+            SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+            des.init(Cipher.ENCRYPT_MODE, key);
 
-    try {
+            getConnection();
 
+            // Use parameterized queries to prevent SQL injection
+            String sql = "SELECT * FROM USER WHERE LOGIN = ? AND PASSWORD = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
 
-      ScriptEngineManager manager = new ScriptEngineManager();
-      ScriptEngine engine = manager.getEngineByName("JavaScript");
-      engine.eval(request.getParameter("module"));
+            resultSet = preparedStatement.executeQuery();
 
-      /* FLAW: Insecure cryptographic algorithm (DES) 
-      CWE: 327 Use of Broken or Risky Cryptographic Algorithm */
-      Cipher des = Cipher.getInstance("DES");
-      SecretKey key = KeyGenerator.getInstance("DES").generateKey();
-      des.init(Cipher.ENCRYPT_MODE, key);
+            if (resultSet.next()) {
 
-      getConnection();
+                login = resultSet.getString("login");
+                password = resultSet.getString("password");
 
-      String sql =
-          "SELECT * FROM USER WHERE LOGIN = '" + login + "' AND PASSWORD = '" + password + "'";
+                User user = new User(login,
+                    resultSet.getString("fname"),
+                    resultSet.getString("lname"),
+                    resultSet.getString("passportnum"),
+                    resultSet.getString("address1"),
+                    resultSet.getString("address2"),
+                    resultSet.getString("zipCode"));
 
-      preparedStatement = connection.prepareStatement(sql);
+                String creditInfo = resultSet.getString("userCreditCardInfo");
+                byte[] cc_enc_str = des.doFinal(creditInfo.getBytes());
 
-      resultSet = preparedStatement.executeQuery();
+                Cookie cookie = new Cookie("login", login);
+                cookie.setMaxAge(864000);
+                cookie.setPath("/");
+                response.addCookie(cookie);
 
-      if (resultSet.next()) {
+                request.setAttribute("user", user.toString());
+                request.setAttribute("login", login);
 
-        login = resultSet.getString("login");
-        password = resultSet.getString("password");
+                LOGGER.info(" User " + user + " successfully logged in ");
+                LOGGER.info(" User " + user + " credit info is " + cc_enc_str);
 
-        User user = new User(login,
-            resultSet.getString("fname"),
-            resultSet.getString("lname"),
-            resultSet.getString("passportnum"),
-            resultSet.getString("address1"),
-            resultSet.getString("address2"),
-            resultSet.getString("zipCode"));
+                getServletContext().getRequestDispatcher
 
-        String creditInfo = resultSet.getString("userCreditCardInfo");
-        byte[] cc_enc_str = des.doFinal(creditInfo.getBytes());
-
-        Cookie cookie = new Cookie("login", login);
-        cookie.setMaxAge(864000);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
-        request.setAttribute("user", user.toString());
-        request.setAttribute("login", login);
-
-        LOGGER.info(" User " + user + " successfully logged in ");
-        LOGGER.info(" User " + user + " credit info is " + cc_enc_str);
-
-        getServletContext().getRequestDispatcher("/dashboard.jsp").forward(request, response);
-
-      } else {
-        request.setAttribute("login", login);
-        request.setAttribute("password", password);
-        request.setAttribute("keepOnline", keepOnline);
-        request.setAttribute("message", "Failed to Sign in. Please verify credentials");
-
-        LOGGER.info(" UserId " + login + " failed to logged in ");
-
-        getServletContext().getRequestDispatcher("/signIn.jsp").forward(request, response);
-      }
-    } catch (Exception e) {
-      throw new ServletException(e);
-    }
-
-  }
-
-  private void getConnection() throws ClassNotFoundException, SQLException {
-    Class.forName("com.mysql.jdbc.Driver");
-    connection = DriverManager.getConnection("jdbc:mysql://localhost/DBPROD", "admin", "1234");
   }
 
 }
