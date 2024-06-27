e7e7e3427efff3c9208c5904f28a8948f4e03ea9
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-06-27 16:32:08 -0400

    Update ServletTarPit.java

--- src/main/java/io/shiftleft/tarpit/ServletTarPit.java
+++ src/main/java/io/shiftleft/tarpit/ServletTarPit.java
@@ -71,9 +71,11 @@ public class ServletTarPit extends HttpServlet {
       getConnection();
 
       String sql =
-          "SELECT * FROM USER WHERE LOGIN = '" + login + "' AND PASSWORD = '" + password + "'";
+          "SELECT * FROM USER WHERE LOGIN = ? AND PASSWORD = ?";
 
       preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setString(1, login);
+      preparedStatement.setString(2, password);
 
       resultSet = preparedStatement.executeQuery();
 
